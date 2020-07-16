5190ebca0b3006e71fade9bcf9347edb3893a43f
Julio Foulquie <julio@thenextweb.com>
2020-07-15 21:02:44 -0300

    Replace variable name to avoid conflicting name with the latest framework version >=7.19.1
    See https://github.com/laravel/framework/commit/0d8faf3d6e8ef1258704a43e5f19dfe7e4a92143
    $config can't be used or it breaks the service provider.

--- config/pass-filesystem.php
+++ config/pass-filesystem.php
@@ -3,21 +3,21 @@
  * If there's no specific filesystem defined for us, let's just copy the default one and change
  * the root.
  */
-$config = config('filesystems');
+$fs_config = config('filesystems');
 
 // Check whether there is a specific filesystem defined for passgenerator
 // or we should use the default one.
-if (isset($config['disks']['passgenerator'])) {
+if (isset($fs_config['disks']['passgenerator'])) {
     $passgenerator_fs = 'passgenerator';
 } else {
-    $passgenerator_fs = $config['default'];
+    $passgenerator_fs = $fs_config['default'];
 }
 
-if (isset($config['disks'][$passgenerator_fs])) {
-    $config['disks']['passgenerator'] = $config['disks'][$passgenerator_fs];
-    $config['disks']['passgenerator']['root'] = storage_path('app/passgenerator');
+if (isset($fs_config['disks'][$passgenerator_fs])) {
+    $fs_config['disks']['passgenerator'] = $fs_config['disks'][$passgenerator_fs];
+    $fs_config['disks']['passgenerator']['root'] = storage_path('app/passgenerator');
 } else {
     throw new Exception('There must be a default filesystem defined.');
 }
 
-return $config;
+return $fs_config;
